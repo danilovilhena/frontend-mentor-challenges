eec289413a52bc5ea0f54bb0b6ec2496a597eb17
Danilo Vilhena <danilo.vilhena@edu.unifor.br>
2021-11-24 23:54:12 -0300

    [sunnyside-landing-page] Correcitng mobile menu

--- sunnyside-landing-page/src/components/Introduction.js
+++ sunnyside-landing-page/src/components/Introduction.js
@@ -7,9 +7,18 @@ import headerImage from "../images/desktop/image-header.png"
 const Introduction = () => {
 
     const clickHandler = () => {
-        let previous = document.querySelector('#mobile-menu').ariaExpanded
-        document.querySelector('#mobile-menu').ariaExpanded = (previous === 'false')
-        document.querySelector('#mobile-menu').ariaHidden = (previous === 'true')
+        let previous = document.querySelector('#mobile-menu').ariaHidden
+        if(previous === 'true') {
+            document.querySelector('#mobile-menu').ariaHidden = 'false'
+            document.querySelector('#mobile-menu').querySelectorAll('*').forEach(el => {
+                el.removeAttribute('tabIndex')
+            })
+        } else {
+            document.querySelector('#mobile-menu').ariaHidden = 'true'
+            document.querySelector('#mobile-menu').querySelectorAll('*').forEach(el => {
+                el.setAttribute('tabIndex', '-1')
+            })
+        }
     }
 
     return (
@@ -27,10 +36,10 @@ const Introduction = () => {
             </nav>
         </header>
         <nav id="mobile-menu" aria-hidden="true">
-            <a href="/#">About</a>
-            <a href="/#">Services</a>
-            <a href="/#">Projects</a>
-            <button>Contact</button>
+            <a tabIndex="-1" href="/#">About</a>
+            <a tabIndex="-1" href="/#">Services</a>
+            <a tabIndex="-1" href="/#">Projects</a>
+            <button tabIndex="-1" >Contact</button>
         </nav>
         <h1>We are creatives</h1>
         <img src={arrowDown} className="arrow" alt="Arrow pointing down"></img>
