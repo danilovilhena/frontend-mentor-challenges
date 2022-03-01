e3a62709fc50394ff2791a9f33c7b7302dcdf35b
Danilo Vilhena <danilo.vilhena@edu.unifor.br>
2022-02-28 22:16:15 -0300

    [witw] Styling grid items

--- where-in-the-world/src/pages/Home.js
+++ where-in-the-world/src/pages/Home.js
@@ -1,8 +1,39 @@
+import {useEffect, useState} from 'react'
+
 import '../styles/Home.scss';
 
 const Home = () => {
+  const [countries, setCountries] = useState([])
+  const pick = (obj, ...keys) => Object.fromEntries(keys.filter(key => key in obj).map(key => [key, obj[key]]));
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v2/all")
+    .then(res => res.json())
+    .then(res => {
+      let filtered = []
+      res.forEach(el => {
+        filtered.push(pick(el, 'name', 'flag', 'population', 'region', 'capital'))
+      })
+      setCountries(filtered)
+    })
+  }, [])
+
   return (
-    <div>Home</div>
+    <main className="container">
+      <div className="grid">
+        {countries.map(el => 
+          <div className="grid-item" key={el.name}>
+            <img src={el.flag} alt={el.name + " flag"} />
+            <div className="grid-content">
+              <h2>{el.name}</h2>
+              <p><b>Population: </b>{el.population}</p>
+              <p><b>Region: </b>{el.region}</p>
+              <p><b>Capital: </b>{el.capital}</p>
+            </div>
+          </div>
+        )}
+      </div>
+    </main>
   );
 }
 
